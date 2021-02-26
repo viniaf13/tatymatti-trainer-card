810801dd819882d3b80c04f090c4c902cb41acac
Vinicius Aguiar <viniaf13@gmail.com>
2021-02-26 15:29:38 -0300

    added gym badges

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,54 @@ class MyApp extends StatelessWidget {
                       ),
                     ),
                   )),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/badge1.png'),
+                  ),
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/badge2.png'),
+                  ),
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/badge3.png'),
+                  ),
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/badge4.png'),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/badge5.png'),
+                  ),
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/badge6.png'),
+                  ),
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/badge7.png'),
+                  ),
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/badge8.png'),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
